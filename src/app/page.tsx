04973311d9be88bed36aa60074388c8a69d13fbb
dragochar <zacyungblut@gmail.com>
2026-09-12 00:56:13 -0400

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default async function Home() {
           Privacy Policy
         </a>
         {/* <a href="/app" className="transition-colors hover:text-[#B9B6A6]">
-          The App yup sdf
+          The App yup sdf sdf
         </a> */}
       </footer>
     </main>
